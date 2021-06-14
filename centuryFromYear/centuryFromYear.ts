32ee1c3c6a1c42845a71e0c2e4731fe5dab77932
Ricky Rozario <rickyfrancisrozario@gmail.com>
2021-06-15 00:11:03 +0600

    Completed: Century From Year

---
+++ centuryFromYear/centuryFromYear.ts
@@ -0,0 +1,13 @@
+function centuryFromYear(year: number): number {
+
+    if(year % 100 === 0) {
+        return Math.floor(year/100)
+    } else {
+        return Math.floor(year/100) + 1
+    }
+ 
+}
+
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1900));
+console.log(centuryFromYear(1700));
